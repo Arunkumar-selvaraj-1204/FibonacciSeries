cb55f7f6b3bd732659d3cea1d9fbd91129d4e782
bala-wk71 <balawk71@gmail.com>
2025-01-03 15:00:07 +0530

    reset

--- Fibonacci.c
+++ Fibonacci.c
@@ -17,9 +17,25 @@ int* FibonacciSeries(int n){
 int sumSeries(int *arr,int n){
     int i,sum=0;
     for(i=0;i<n;i++){
+        
         sum+=arr[i];
     }
-    return sum-1;
+    return sum+1;
+}
+
+int* FibonacciSeriesEvenNumbers(int* arr, int n){
+    int i,size=0;
+    for(i=0;i<n;i++){
+        if(arr[i]%2==0) size++;
+    }
+    int* even = (int*)malloc(size*sizeof(int));
+    int j=0;
+    for(i=0;i<n;i++){
+        if(arr[i]%2==0){
+            even[j++]=arr[i];
+        } 
+    }
+    return even;
 }
 
 int main()
@@ -30,4 +46,8 @@ int main()
     
     int sumOfFib= sumSeries(array,n);
     printf("%d",sumOfFib);
+
+    printf("\n");
+    int* even=FibonacciSeriesEvenNumbers(array,n);
+    for(int i=0; i<(sizeof(even)/sizeof(even[0])); i++) printf("%d ", even[i]);
 }
